f284e529743f9f226efb03ac6ba5a1d9ad94aab0
Damir <damirr765@gmail.com>
2023-09-10 00:39:43 +0300

    add: SearchBar widget

--- app/src/main/java/com/kernel/miner/MainActivity.kt
+++ app/src/main/java/com/kernel/miner/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,11 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kernel.miner.ui.theme.MinerTheme
@@ -32,22 +31,46 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			MinerTheme {
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
 					Column(
-						modifier = Modifier.padding(start = 16.dp, top = 56.dp, end = 16.dp, bottom = 16.dp),
+						modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp),
 						verticalArrangement = Arrangement.Top,
-						horizontalAlignment = Alignment.CenterHorizontally
+						horizontalAlignment = Alignment.Start
 					) {
-						Text(
-							"Some string here", style = TextStyle(
-								color = MaterialTheme.colorScheme.primary,
-								fontWeight = FontWeight(400),
-								fontSize = 16.sp
-							)
-						)
+						SearchBar()
 					}
 				}
 			}
 		}
+	}
+}
+
+
+@Composable
+fun SearchBar(){
+	Row(
+		horizontalArrangement = Arrangement.SpaceBetween,
+		verticalAlignment = Alignment.CenterVertically,
+		modifier = Modifier
+			.background(color = Color(0xFFF2F2F2), shape = RoundedCornerShape(size = 999.dp))
+			.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+			.fillMaxWidth()
+	) {
+		Text(
+			text = "Enter tracking number...",
+			style = TextStyle(
+				fontSize = 16.sp,
+				fontWeight = FontWeight(400),
+				color = Color(0x8F000000),
+			),
+
+			)
+		Image(
+			painter = painterResource(id = R.drawable.search_outline),
+			contentDescription = "search description"
+		)
 	}
 }
